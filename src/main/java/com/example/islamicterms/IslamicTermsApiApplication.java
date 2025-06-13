938074ada7b6567f5bb39e9f9c89b4e910730712
Rustam Suleymanov <dev.suleymanov@gmail.com>
2025-06-13 15:14:02 +0300

    first commit

---
+++ src/main/java/com/example/islamicterms/IslamicTermsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.islamicterms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslamicTermsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslamicTermsApiApplication.class, args);
+	}
+
+}
