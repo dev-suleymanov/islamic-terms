49f5a520f2cceaab75ae8d19d4f4f17e9e318662
Rustam Suleymanov <dev.suleymanov@gmail.com>
2025-06-16 14:04:34 +0300

    a test commit for git stash and further git rebase

---
+++ src/main/java/com/example/islamicterms/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.islamicterms.repository;
+
+import com.example.islamicterms.model.Category;
+
+import java.util.Optional;
+
+public interface CategoryRepository {
+    Optional<Category> findByName(String name);
+    boolean existsByName(String name);
+}
